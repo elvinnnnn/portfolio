a26c878139809456a27813ec28c75ce4714942c5
elvinnnnn <79492554+elvinnnnn@users.noreply.github.com>
2024-10-19 15:49:36 +1100

    small tweaks to resume, projects responsiveness, styles.

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,42 +1,44 @@
 import React, { useState } from "react";
 import GitHub from "./GitHub";
 import Pdf from "./Pdf";
+
+interface Project {
+  description: string;
+  tech: string;
+  link: string;
+  img: string;
+}
+
 function Projects() {
   const [active, setActive] = useState<string>("AInterview");
-  type Project = {
-    description: string;
-    tech: string;
-    github: string;
-    img: string;
-  };
 
-  const projectList: { [key: string]: Project } = {
+  const projects: { [key: string]: Project } = {
     AInterview: {
       description:
-        "AI chat bot application that performs mock interviews, and gives feedback based on user responses.",
+        "AI chat bot app that performs mock interviews, and gives feedback based on user responses.",
       tech: "ASP.NET Core, TypeScript, NextJS, TailwindCSS, OpenAI API",
-      github: "https://github.com/elvinnnnn/AInterview",
+      link: "https://github.com/elvinnnnn/AInterview",
       img: "/ainterview.png",
     },
     FinConnect: {
       description:
         "Financial application that provides correlation analysis based on stock price and news articles.",
       tech: "Python, GraphQL, JavaScript, ReactJS, TailwindCSS, Bootstrap, New Relic",
-      github: "/finconnect.pdf",
+      link: "/finconnect.pdf",
       img: "/finconnect.png",
     },
     Songdle: {
       description:
         "Wordle inspired audio recognition mini game based on an artist's discography.",
       tech: "TypeScript, NextJS, Youtube API, Spotify API, TailwindCSS",
-      github: "https://github.com/elvinnnnn/songdle",
+      link: "https://github.com/elvinnnnn/songdle",
       img: "/songdle.png",
     },
     "Introvert in IKEA": {
       description:
         "IKEA inspired survival/stealth game developed for both 3D/Desktop and VR.",
       tech: "Unreal Engine, C++, Blueprints, Blender, HTC Vive",
-      github: "/?=projects",
+      link: "/?=projects",
       img: "/introvertinikea.png",
     },
   };
@@ -45,8 +47,8 @@ function Projects() {
     <div className="page-container">
       <div className="page-title">projects.&nbsp;</div>
       <div className="page-content projects">
-        {Object.keys(projectList).map((key) => {
-          const project = projectList[key];
+        {Object.keys(projects).map((key) => {
+          const project = projects[key];
           return (
             <div key={key} className="project-container">
               {key === active ? (
@@ -66,9 +68,9 @@ function Projects() {
                     <div className="project-tech">{project.tech}</div>
                     <div className="project-redirect">
                       {key === "FinConnect" ? (
-                        <Pdf link={project.github} name={"finconnect_report"} />
+                        <Pdf link={project.link} name={"finconnect_report"} />
                       ) : (
-                        <GitHub link={project.github} />
+                        <GitHub link={project.link} />
                       )}
                     </div>
                   </div>
